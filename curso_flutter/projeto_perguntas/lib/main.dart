97949145cb2d0cb962e12d4e596d7287276d12b7
Rafael Santana <oficialrsantana@gmail.com>
2023-03-29 02:01:53 -0300

    commit 2

--- curso_flutter/projeto_perguntas/lib/main.dart
+++ curso_flutter/projeto_perguntas/lib/main.dart
@@ -1,13 +1,56 @@
 import 'package:flutter/material.dart';
+import './questao.dart';
 
 void main() {
   runApp(PerguntaApp());
 }
 
-class PerguntaApp extends StatelessWidget {
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+
+  void _responder() {
+    setState(() {
+      _perguntaSelecionada++;
+    });
+    print(_perguntaSelecionada);
+    if (_perguntaSelecionada > 1) {
+      _perguntaSelecionada = 0;
+    }
+  }
+
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito'
+    ];
     return MaterialApp(
-      home: Text('Olá Flutter!!!'),
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Perguntas'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Questao(perguntas[_perguntaSelecionada]),
+          RaisedButton(
+            child: Text('Resposta 1'),
+            onPressed: _responder,
+          ),
+          RaisedButton(
+            child: Text('Resposta 2'),
+            onPressed: _responder,
+          ),
+          RaisedButton(
+            child: Text('Resposta 3'),
+            onPressed: _responder,
+          ),
+        ],
+      ),
+    ));
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
   }
 }
